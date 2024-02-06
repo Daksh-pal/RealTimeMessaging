7811a3c52f157dc420848807f0409d293a7ece94
Daksh Pal <pal.daksh22@gmail.com>
2024-02-06 19:39:45 +0530

    Set the environment and declare DB models

---
+++ backend/controllers/authController.js
@@ -0,0 +1,13 @@
+export const login = (req,res) =>{
+    console.log("Logged in")
+}
+
+export const logout = (req,res) =>{
+    console.log("Logged out")
+}
+
+export const signup =async (req,res) =>{
+    // try {
+    //     const {fullname , username , password , confirmpassword , gender} = req.body;
+    // } catch (err) {
+}
---
+++ backend/server.js
@@ -0,0 +1,17 @@
+import  express  from 'express';
+import authRoutes from "./routes/authRoutes.js"
+import dotenv from 'dotenv';
+import connectToMongoDB from './db/connectToMongoDB.js';
+dotenv.config();
+const app = express();
+
+
+
+const PORT = process.env.PORT || 3000;
+
+app.use('/api/auth',authRoutes)
+
+app.listen(PORT,()=>{
+    connectToMongoDB();
+    console.log("Server Open")
+})
